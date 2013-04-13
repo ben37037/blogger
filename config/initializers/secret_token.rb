caf6cedda07e39869df5f46a4b50a4d348840c1f
Rails Rookies Bootcamp by Synaptian <railsrookies@rails-rookies-bootcamp-by-synaptian.(none)>
2013-04-13 10:52:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '0455656a0ab32de47a7d6af2d9990cd340cdd42e25bf40bd16c1eeb34d4232189185a1844d6c9556a83908dc90c6cba4fb19f249ed2b4b98a0a1378c81fa1846'
